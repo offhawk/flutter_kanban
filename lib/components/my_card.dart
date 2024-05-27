4b3fa8e750e601361e58848bd9f3758794ffa4c8
offhawk <notorioushawk123@gmail.com>
2024-05-26 23:12:36 -0300

    UI improvement

--- lib/components/my_card.dart
+++ lib/components/my_card.dart
@@ -5,13 +5,20 @@ import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
 class MyCard extends StatefulWidget {
   final Task task;
-  const MyCard({super.key, required this.task});
+  final void Function(Task task) parentRemoveList;
+  const MyCard({super.key, required this.task, required this.parentRemoveList});
 
   @override
-  State<MyCard> createState() => _MyCardState();
+  State<MyCard> createState() => _MyCardState(parentRemoveList: this.parentRemoveList);
 }
 
 class _MyCardState extends State<MyCard> {
+  final void Function(Task task) parentRemoveList;
+
+  List<Color> colors = <Color>[Color(0xffffffff), Color(0xfffbb732), Color(0xff7ed998)];
+
+  _MyCardState({required this.parentRemoveList});
+
   @override
   Widget build(BuildContext context) {
     return DragItemWidget(
@@ -24,18 +31,38 @@ class _MyCardState extends State<MyCard> {
        allowedOperations: () => [DropOperation.copy],
       child: DraggableWidget(
         child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                     width: double.infinity,
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border(
+                        bottom: BorderSide(
+                          color: colors[widget.task.type ?? 0],
+                          width: 4
+                        )
+                      )
                     ),
-                    child: Text('${widget.task.desc}',
-                    style:  GoogleFonts.bricolageGrotesque(
-                            color: Color(0xff2c303f),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Text('${widget.task.desc}',
+                            style:  GoogleFonts.bricolageGrotesque(
+                                    color: Color(0xff2c303f),
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                         IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Color(0xff2c303f),
+                          tooltip: 'Deletar',
+                          onPressed: () {
+                            parentRemoveList(widget.task);
+                          }),
+                      ],
+                    ),
                   ),
       ),
     );
